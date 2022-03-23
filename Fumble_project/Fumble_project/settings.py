1fa49f59096fc8a704703608571b3e9b90ba1f37
Brandon N <bnguyen4@umbc.edu>
2022-03-22 23:18:24 -0400

    Update settings.py

--- Fumble_project/Fumble_project/settings.py
+++ Fumble_project/Fumble_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6b8&q5w)g_dirufo%9#$_%2wy+j!vlkxiyzq)eco)@8zb0r$x'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
