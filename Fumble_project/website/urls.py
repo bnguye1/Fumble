8c171d95c7cbc16aa14e6eddef06c546a864d027
Brandon Nguyen <bnguyen4@umbc.edu>
2022-04-12 18:10:56 -0400

    Added login and register page redirects

--- Fumble_project/website/urls.py
+++ Fumble_project/website/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='website-home'),
     path('about/', views.about, name='website-about'),
-    path('login/', views.login, name='website-login')
+    path('login/', views.login, name='website-login'),
     path('register/', views.register, name='website-register')
 ]
