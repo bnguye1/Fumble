52f6e3afd6bac7259808b7f8ce76e8ec4d1e009c
Xiro234 <xiro234@gmail.com>
2022-04-23 20:41:34 -0400

    Added tests

--- Fumble_project/website/tests.py
+++ Fumble_project/website/tests.py
@@ -3,10 +3,23 @@ from selenium import webdriver
 
 
 class RegistrationTest(TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        pass
 
-    def testForm(self):
-        selenium = webdriver.Chrome()
+    def testDoInputsExist(self):
+        driver = self.driver
+
         # URL
-        selenium.get('http://127.0.0.1:8000/register/')
+        driver.get('http://127.0.0.1:8000/register/')
 
+        self.assertNotEqual(driver.find_elements('usernameBox'), 0)
+        self.assertNotEqual(driver.find_elements('emailBox'), 0)
+        self.assertNotEqual(driver.find_elements('passwordBox'), 0)
+        self.assertNotEqual(driver.find_elements('tosCheck'), 0)
+        self.assertNotEqual(driver.find_elements('registerButton'), 0)
 
+        driver.close()
+
+    def tearDown(self):
+        self.driver.close()
