ed4e1361779c5ed40dbaf81d345d05841231661e
nalgreen <agreen11@umbc.edu>
2022-04-25 18:50:32 -0400

    changed settings.py configs for databases section, added .env file w/secret key

--- Fumble_project/Fumble_project/settings.py
+++ Fumble_project/Fumble_project/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'fumbleDB',
-        'USER': 'fumble',
-        'PASSWORD': 'fumble',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
